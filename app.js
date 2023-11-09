140f6df4493a05dcf03bc5a9c5490be80ad06d26
Christopher Devalme <christhecreative@christophersmbp.myfiosgateway.com>
2023-11-08 19:36:31 -0500

    added real esate and portfolio options

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ console.log('App is connected');
 const barbie = {
     name: 'Barbie',
     wardrobe: [],
+    portfolio: [],
     wallet: 0
 }
 
@@ -80,10 +81,17 @@ class Clothing {
     }
 }
 
+class RealEstate {
+    constructor(name, cost){
+        this.name = name;
+        this.cost = cost;
+    }
+}
+
 const birkin = new Clothing('Birkin Bag', 'Hermes', 'purple', 'bag', 'lg', 15470 )
 const chanel = new Clothing('Chanel bag', 'Chanel', 'black', 'bag', 'med', 35000 )
 const redBottoms = new Clothing('Red Bottoms', 'Christian Loboutin', 'black', 'shoes', '6', 3000)
-
+const house = new RealEstate('Malibu Condo', 50000)
 
 // Game Screen
 
@@ -106,6 +114,18 @@ barbie.render = () => {
             </li>`
         })).join('')
     }</ul>
+    </div>
+    <div>
+    <h2>Real Estate portfolio contains:</h2>
+    <ul>${
+        barbie.portfolio.map((item => {
+            return `<li>
+            ${barbie.name} has a ${item.name} worth $${item.cost}.
+            </li>`
+        })).join('')
+    }
+    
+    </ul>
     </div>
 `;
 }
@@ -136,6 +156,10 @@ workButton.addEventListener('click', ()=>{
     barbie.wallet += barbie.career.income; // WE updated the wllet that belongs to barbie so the object was changed
     // the object control the information that is visible to us on the screen
     // I want to re-render the content so that i can see the updated information in the browser
+    
+    if(barbie.portfolio.length > 0){
+        barbie.wallet += barbie.portfolio.length * 500
+    }
     barbie.render();
 })
 
@@ -161,6 +185,21 @@ chanelButton.addEventListener('click' , () => {
     if(barbie.wallet >= chanel.price){
         barbie.wardrobe.push(chanel);
         barbie.wallet -= chanel.price;
+        barbie.render();
+        // WE updated the wardrobe that belongs to barbie so the object was changed
+    // the object control the information that is visible to us on the screen
+    // I want to re-render the content so that i can see the updated information in the browser
+    } else {
+        alert('Stop trippin you know you aint got it like that');
+    }
+})
+
+const houseButton = document.getElementById('house');
+
+houseButton.addEventListener('click', () => {
+    if(barbie.wallet >= house.cost){
+        barbie.portfolio.push(house);
+        barbie.wallet -= house.cost;
         barbie.render();
         // WE updated the wardrobe that belongs to barbie so the object was changed
     // the object control the information that is visible to us on the screen
